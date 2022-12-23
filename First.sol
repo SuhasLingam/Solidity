cff83b3c593c7cfda50c6355dff97efa0102809d
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2022-12-23 20:09:07 +0530

    First :thumb:

---
+++ First.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNIDENTIFIED
+
+pragma solidity >0.8.12;
+
+contract firstCode {
+
+    uint256 public num;
+
+    function NewNum(uint256 _UpdatedNum) public {
+        num = _UpdatedNum;
+    }
+
+}
