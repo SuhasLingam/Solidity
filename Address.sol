befe2104837a49ed735852bc370c6548d5bcf99a
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2022-12-23 20:26:27 +0530

    Add

---
+++ Address.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNIDENTIFIED
+
+pragma solidity >0.8.12;
+
+contract aboutAddress {
+
+    address public Myaddress;
+
+    function NewAddress(address _newAdd) public {           // To Change Address
+        Myaddress = _newAdd;
+    }
+    
+    function CheckBalance() public view returns(uint){
+        return Myaddress.balance;
+    }
+}
