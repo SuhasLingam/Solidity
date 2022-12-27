9af6127a21027444f2ad9776de64083899f05aa3
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2022-12-28 00:36:18 +0530

    receive and fallback Function:white_check_mark:

--- new.sol
+++ new.sol
@@ -1,1 +1,25 @@
-// address in solidity?
+//SPDX-License-Identifier: MIT
+
+pragma solidity >0.8.13;
+
+
+contract MyContract {
+
+
+    uint MyValue;
+    string str1;
+    string str2;
+
+    receive() external payable {
+
+        MyValue = msg.value;
+        str1 = "Receive Function";
+    }
+
+    fallback() external payable {
+        MyValue = msg.value;
+        str2 = "Fallback Function";
+    }
+
+
+}
