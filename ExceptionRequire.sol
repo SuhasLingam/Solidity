d9ce95408a439813c1e879fe7fe477b1318c21c4
Suhas Srinivas Lingam <95020208+SuhasLingam@users.noreply.github.com>
2023-02-23 02:51:37 +0530

    ExceptionRequire :white_check_mark:

---
+++ ExceptionRequire.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >0.7.0;
+
+
+contract Exceptionsrequire {
+
+    mapping (address => uint) public BalanceRecived;
+    
+
+    function Deposit() public payable {
+        BalanceRecived[msg.sender] += msg.value;
+    }
+
+    function withdraw(address payable _to , uint _amount) public {
+        
+       // if(_amount <= BalanceRecived[msg.sender]) { ---- } instead of if() we use require 
+
+        require(_amount <= BalanceRecived[msg.sender], "Cannot Transfer Funds , Not Enough !!!");
+        BalanceRecived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+        
+        
+    }
+    
+}
